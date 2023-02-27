4d98d0ba26d72cbf730d59e5f5c7608c7ebb3519
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-02-28 01:22:47 +0300

    work environment improvement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    indent: ['error', 2, {VariableDeclarator: 1}],
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+  },
+  plugins: ['unused-imports'],
 };
