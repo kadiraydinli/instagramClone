5ff85536d8d465eccb4030c7f3463cde8cdc918a
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-02-28 21:23:09 +0300

    email validate regex changed

--- src/utils/emailValidate.ts
+++ src/utils/emailValidate.ts
@@ -1,4 +1,4 @@
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailRegex = /^[a-z0-9.]{1,64}@[a-z0-9.]{1,64}$/i;
 
 export const emailValidate = (email: string): boolean => {
     return emailRegex.test(email);
