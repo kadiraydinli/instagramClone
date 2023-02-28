8019c436e72b8ff0d758c5c63205eeabbea953d8
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-02-28 22:07:05 +0300

    home screen header fix

--- src/screens/BottomNavigation/Home.tsx
+++ src/screens/BottomNavigation/Home.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { BottomTabParamList } from 'navigation/BottomNavigator';
+import { HomeHeader } from 'components';
 
 type Props = NativeStackScreenProps<BottomTabParamList, 'Home'>;
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <HomeHeader />
       <Text>Home Screen</Text>
       <Button title="Back to Login" onPress={() => navigation.pop()} />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
   },
 });
