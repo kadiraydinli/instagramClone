bf62a7c59ad4249c1e3887ccd9c4664a40966f70
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-03-02 19:50:55 +0300

    react-native-system-navigation-bar installed and used

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import RNBootSplash from 'react-native-bootsplash';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
 import { PersistGate } from 'redux-persist/integration/react';
 import { RootNavigator } from 'navigation';
 import { persistor, store } from 'store/store';
+import { palette } from 'theme';
 
 RNBootSplash.hide({ fade: true });
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SystemNavigationBar.setNavigationColor(palette.white, 'dark', 'both');
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
